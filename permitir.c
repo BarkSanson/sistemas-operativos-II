255962420ba9b53301b72e4fff62a8df952671f5
Legoruu <13arnauvidal@gmail.com>
2022-04-24 17:47:51 +0200

    hecho permitir.c

--- permitir.c
+++ permitir.c
@@ -1,4 +1,26 @@
+#include "ficheros_basico.h"
+#include "ficheros.c"
 
 int main(int argc, char** argv) {
-    return 0;
+    char *path;
+    int ninodo;
+
+    if(argc != 3) {
+        fprintf(stderr, "Número de argumentos inválido, son necesarios 3 argumentos: [nombre_dispositivo] [número_de_inodo] [permisos_inodo]");
+        return EXIT_FAILURE;
+    }
+     
+    //montamos el dispositivo 
+    path = argv[1];
+    bmount(path);
+
+    //transformamos a int el numero de inodo
+    ninodo = atoi(argv[2]);
+
+    //llamamos a la funcion mi_chmod_f dandole el numero de inodo
+    //y sus correspondientes permisos
+    mi_chmod_f(ninodo,argv[3]);
+
+    //desmontamos el dispositivo
+    bumount();
 }
